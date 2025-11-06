397fc94fe8472e776262ced2eb41138acf2514bb
Samuel Ribeiro <samuelhunter990@gmail.com>
2025-11-06 18:31:41 -0300

    Add files via upload

---
+++ valor.py
@@ -0,0 +1,11 @@
+x = float(input("What's x ?"))
+y= float(input("What's y ?"))
+
+if x < y:
+    print("x is less than y")
+
+elif x > y:
+    print("x is greater than y")
+
+else:
+    print("x is equal than y")
